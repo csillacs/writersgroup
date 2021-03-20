b68c157a5cb278f2128fee23ea69f8896111d22b
Csilla Csépke <csillacs@users.noreply.github.com>
2021-03-20 09:54:05 +0200

    configure purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+    "./src/**/*.css",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
