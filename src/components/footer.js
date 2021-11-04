9d77625fd1c47cdbb21406c92ac78eacb0f07ab7
Csilla Csépke <csillacs@users.noreply.github.com>
2021-11-04 20:39:31 +0200

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -44,7 +44,7 @@ export default function Footer() {
 
             <a
               className="my-3 block"
-              href="https://helsinki.zoom.us/j/61067209350"
+              href="https://us02web.zoom.us/j/87003211410?pwd=QVIxeUhpYjcvc2tQQ21VWC93enkwdz09&fbclid=IwAR33S1UeFMrCp4JrOyekkrXG0-05vFiTAEulR9jmyV_BN5pcfdqYUiG0ilE#success"
               target="_blank"
               rel="noreferrer"
             >
